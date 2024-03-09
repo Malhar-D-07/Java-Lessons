d6e86697f2f9997dcd0e0c6d0f6577eec6fbae01
Malhar <malhar@higgsbosonhealth.com>
2024-03-09 23:30:20 +0530

    Adding and Subtracting using ExecutorService

--- src/AdderSubtractor/Main.java
+++ src/AdderSubtractor/Main.java
@@ -1,17 +1,27 @@
 package AdderSubtractor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
     public static void main(String [] args) throws Exception {
 
+
+        ExecutorService es = Executors.newFixedThreadPool(10);
         Value v = new Value();
 
         Adder adder = new Adder(v);
-        adder.call();
+        Future<Void> f1 = es.submit(adder);
         System.out.println("After Adding: " + v.i);
 
         Subtractor sub = new Subtractor(v);
-        sub.call();
+        Future<Void> f2 = es.submit(sub);
         System.out.println("After Subtracting: " + v.i);
+
+        f1.get();
+        f2.get();
+        System.out.println("Final Value: " + v.i);
 
     }
 }
