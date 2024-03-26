036ac969971e79eae303f93b20b47bbea5c11b2f
Malhar <malhar@higgsbosonhealth.com>
2024-03-26 20:32:10 +0530

    Synchronised thread safe Singleton class

--- src/DesignPatterns/Singleton.java
+++ src/DesignPatterns/Singleton.java
@@ -7,7 +7,17 @@ public class Singleton {
 
     // This code is not thread safe. Suppose 2 threads access if condition of getInstance method at same instance
     // then 2 instances will be created.
-    public static Singleton getInstance() {
+
+//    public static Singleton getInstance() {
+//        if(instance == null) {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+
+    // One way to make it thread safe is to make it synchronised
+    // Now, if this method would have 1000 lines, then making it synchronized would make the method slow
+    public static synchronized Singleton getInstance() {
         if(instance == null) {
             instance = new Singleton();
         }
