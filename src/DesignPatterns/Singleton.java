f8237f061eda4deb2025f1c2b61f365f40d6d2f0
Malhar <malhar@higgsbosonhealth.com>
2024-03-26 20:47:01 +0530

    Thread safe solutions using - Locks - Locks with Double Check

--- src/DesignPatterns/Singleton.java
+++ src/DesignPatterns/Singleton.java
@@ -1,7 +1,11 @@
 package DesignPatterns;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Singleton {
     private static Singleton instance = null;
+    static Lock lock = new ReentrantLock();
     private Singleton() {
     }
 
@@ -17,9 +21,33 @@ public class Singleton {
 
     // One way to make it thread safe is to make it synchronised
     // Now, if this method would have 1000 lines, then making it synchronized would make the method slow
-    public static synchronized Singleton getInstance() {
+//    public static synchronized Singleton getInstance() {
+//        if(instance == null) {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+
+    // Another way is to find the CRITICAL SECTION and apply lock on it. Here the CRITICAL SECTION is the IF
+    // condition block. This code will create lock for every time it is called. If we want to avoid this
+    // we can use DOUBLE CHECK SOLUTION
+//    public static Singleton getInstance() {
+//        lock.lock();
+//            if(instance == null) {
+//                instance = new Singleton();
+//            }
+//        lock.unlock();
+//        return instance;
+//    }
+
+    //  DOUBLE CHECK SOLUTION: This will acquire lock only if instance is null
+    public static Singleton getInstance() {
         if(instance == null) {
-            instance = new Singleton();
+            lock.lock();
+            if(instance == null) {
+                instance = new Singleton();
+            }
+            lock.unlock();
         }
         return instance;
     }
